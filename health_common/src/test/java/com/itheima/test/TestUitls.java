eee9d9a07577ec1d1b5a84eb66c7b01a92e34769
zengjx <1164512033@qq.com>
2019-10-26 21:02:35 +0800

    2019.10.21.01 zengjx   添加TestUtils.java

---
+++ health_common/src/test/java/com/itheima/test/TestUitls.java
@@ -0,0 +1,15 @@
+package com.itheima.test;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author zengjx
+ * @Company zengjx
+ * @Date 2019/10/26  20:59
+ * @Version V1.0
+ */
+public class TestUitls {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaazengjx");
+    }
+}
