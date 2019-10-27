89c63cf56ea28894702fd6de32e67cab350fb66b
zengjx <1164512033@qq.com>
2019-10-27 09:15:35 +0800

    2019.10.27.    zengjx   第一次提交

--- health_common/src/test/java/com/itheima/test/TestUitls.java
+++ health_common/src/test/java/com/itheima/test/TestUitls.java
@@ -11,5 +11,6 @@ package com.itheima.test;
 public class TestUitls {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaazengjx");
+        System.out.println("aaaaaaaaaaaaaazengjx");
     }
 }
