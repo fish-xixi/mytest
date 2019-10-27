e2f39031ca6efd3e7897acdac48203acada85fea
werhh <7818>
2019-10-27 11:12:04 +0800

    新建测试类

--- health_common/src/test/java/com/itheima/test/Test_zly.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.test;
-
-/**
- * 钟立宇的测试类
- */
-
-public class Test_zly {
-}
---
+++ health_common/src/test/java/com/itheima/test/huanghaiTest.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+/**
+ * @Author hh
+ * @Date 2019/10/27  11:01
+ */
+public class huanghaiTest {
+    public static void main(String[] args) {
+        System.out.println("  测试用例。。。");
+    }
+}
